e58213e973162d2987180fbae43bfe4e65910deb
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-06-20 18:42:42 +0300

    First

---
+++ prefix_tree.rb
@@ -0,0 +1,19 @@
+class Node
+  attr_accessor :node_value, :node_childs
+
+  def initialize(node_value, node_childs = [])
+    @node_value = node_value
+    @node_childs = node_childs
+  end
+end
+
+class Trie
+  def add
+
+  end
+
+  def includes?
+
+  end
+
+end

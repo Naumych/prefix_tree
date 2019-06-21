6da3dcb946afd76357659762413ae439a4926e00
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-06-21 18:35:37 +0300

    первый пятничный

--- prefix_tree.rb
+++ prefix_tree.rb
@@ -1,21 +1,22 @@
 class Node
-  attr_accessor :node_value, :node_childs
+  attr_accessor :node_value #, :node_childs
+  attr_reader :node_childs
 
-  def initialize(node_value, node_childs = nil)
+  def initialize(node_value, node_childs = [])
     @node_value = node_value
-    @node_childs_array = node_childs
+    @node_childs = node_childs # тут должен быть вызов функции node_childs, происходит ли он?
   end
 
-  def node_childs_array=(node_child)
-    node_childs_array.push(node_child)
+  def node_childs=(node_childs)
+    node_childs.push(node_child)
   end
 end
 
 class Trie
   #attr_accessor :trie
 
-  def initialize(node_first_childs_value)
-    @node_first = Node.new(nil, node_first_childs_value)
+  def initialize()
+    @first_node_in_trie = Node.new(nil ) #тут вторым ваще-то должна быть ссылка на нижнего
 
   end
 
@@ -24,7 +25,7 @@ class Trie
     trie = self.new(word[i])
 
     while i < word.length-2
-      node=Node.new(word[i],word[i+1])
+      node=Node.new(word[i],word[i+1]) #вопрос хранить ли тут объекты или их значения
 
 
   end
